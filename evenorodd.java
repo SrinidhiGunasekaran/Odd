d677885a44fdc826b4721c5daa7f2dc68155a688
SrinidhiGunasekaran <srirajeeg@gmail.com>
2016-10-21 09:48:11 +0530

    Create evenorodd.java

---
+++ evenorodd.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class EvenOdd
+{
+public static void main(String args[])
+{
+int num;
+Scanner sc=new Scanner(System.in);
+num=sc.nextInt();
+if(num%2==0)
+{
+System.out.println("Even Number");
+}
+else if(num%2!=0)
+{
+System.out.println("Odd Number");
+}
+}
+}
